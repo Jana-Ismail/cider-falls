877669d55a9a4496c10eac00aad251efcdc6de7c
Jana Ismail <jana.ismail.nss@gmail.com>
2025-03-05 20:20:41 -0600

    Created html boilerplate, set up main file structure for project, and commented responsibility of each JS module

---
+++ scripts/main.js
@@ -0,0 +1,4 @@
+// This module will generate the html structure for the main container,
+// it will utilize the html component functions to generate and situate html dynamically from the database object,
+// it will grab hold of the #main-container in the DOM,
+// and it will update the innerHTML of the #main-container with the newly generated html.
