f06c750e967aba683f1948dbbcb48ce406dd3bee
Jana Ismail <jana.ismail.nss@gmail.com>
2025-03-06 10:59:09 -0600

    Defined findParkAreaServices and getServicesData functions

--- scripts/ParkAreaServices.js
+++ scripts/ParkAreaServices.js
@@ -1,4 +1,24 @@
 // Import ParkAreas, Services, and ParkAreaServices getter functions from database.js
+// import { getServices, getParkAreaServices } from "./database.js";
 
-// Declare services, parkAreas, and parkAreaServices variables and invoke the appropriate imported getter function,
-// assigning the return value of the getter function to the newly declared variable
+// Define and export a function that takes in the id of one Park Area and the copy of the parkAreaServicesArr as parameters
+// that will filter out all parkAreaService objects that have a parkAreaId value that matches the parkAreaId passed in
+export const findParkAreaServices = (parkAreaId, parkAreaServicesArr) => {
+    const foundParkAreaServices = parkAreaServicesArr.filter(parkAreaService => parkAreaId = parkAreaService.parkAreaId)
+
+    return foundParkAreaServices
+}
+
+export const getServicesData = (servicesArr, foundParkAreaServicesArr) => {
+    const parkAreaServiceData = []
+
+    for (const parkAreaService of foundParkAreaServicesArr) {
+        for (const service of servicesArr) {
+            if (parkAreaService.serviceId = service.id) {
+                parkAreaServiceData.push(service)
+            }
+        }
+    }
+
+    return parkAreaServiceData
+}
